1bb30da87ee18ae2b250e2e65c6f19e2911a644e
iiGray <iiiigray19@gmail.com>
2025-10-26 17:00:05 +0800

    update `get_actor_cls`: add argument collate_type

--- loomtrain/core/modeling/actors.py
+++ loomtrain/core/modeling/actors.py
@@ -1,6 +1,13 @@
 import torch
 from torch import nn
-from typing import Optional
+
+from typing import Literal, Optional
+from functools import partial
+from transformers.modeling_outputs import (
+    BaseModelOutputWithPast, 
+    SequenceClassifierOutputWithPast
+)
+
 from loomtrain.core.actor import (
     LoomActor
 )
@@ -8,11 +15,12 @@ from loomtrain.core.parallel import parallel_state as parallel
 from loomtrain.core.modeling.customs.rm_modeling import train_forwards
 
 
-def get_actor_cls(actor_type: Literal["causal", "classifier"] = "causal") -> "GPT" | "RM":
-    if actor_type == "causal":
+def get_actor_cls(actor_type: Literal["causal", "classifier"] = "causal", 
+                  collate_type: Literal["packing", "padding"] = "packing") -> "PackingGPT" | "PackingRM":
+    if (actor_type, collate_type) == ("causal", "packing"):
         return PackingGPT
     
-    elif actor_type == "classifier":
+    elif (actor_type, collate_type) == ("classifier", "packing"):
         return PackingRM
 
 
@@ -38,7 +46,7 @@ class PackingGPT(Actor):
         return output
 
 class PackingRM(Actor):
-    def __init__(self, model):
+    def __init__(self, model: "nn.Module"):
         model._org_forward = model.forward
         
         train_forward = getattr(train_forwards,  model.config.architectures[0],
@@ -54,7 +62,7 @@ class PackingRM(Actor):
                       input_ids: torch.LongTensor = None, #packed_sequences
                       attention_mask: Optional[torch.BoolTensor] = None,
                       position_ids: Optional[torch.LongTensor]= None,                     
-                      model = None,
+                      model: "nn.Module" = None,
                       ** kwargs,
                       ):
 
@@ -73,12 +81,12 @@ class PackingRM(Actor):
 
 
     def forward(self,
-                sequences: torch.LongTensor, #packed_sequences
-                seq_lens: Optional[list[int]] = None,
-                attention_mask: Optional[torch.BoolTensor] = None,
+                sequences: "torch.LongTensor", #packed_sequences
+                seq_lens: "Optional[list[int]]" = None,
+                attention_mask: "Optional[torch.BoolTensor]" = None,
                 ):
 
-        inputs = parallel.prepare_cp_input(sequeces = sequeces, 
+        inputs = parallel.prepare_cp_input(sequeces = sequences, 
         seq_lens = seq_lens, attention_mask = attention_mask)
         output = self.model(*inputs)
 
