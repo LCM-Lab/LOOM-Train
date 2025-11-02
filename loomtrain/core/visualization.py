7e2a50f03dc8d9ecde19ff147dc22b09f8414eb6
iiGray <iiiigray19@gmail.com>
2025-11-02 15:54:18 +0800

    reimplement VisualizationModule: easier to use

--- loomtrain/core/visualization.py
+++ loomtrain/core/visualization.py
@@ -1,8 +1,9 @@
 import os, wandb
+from typing import Literal
 from dataclasses import dataclass
 import torch
 from torch.utils.tensorboard import SummaryWriter
-
+from loomtrain.core.utils import basename, dirname
 from loomtrain.core.state import CheckpointMixin
 from loomtrain.core.utils import (
     IO, rank0only_decorator, rank0print,
@@ -28,15 +29,28 @@ class TensorboardConfig:
 
 
 class VisualizationModule(CheckpointMixin):
+    '''Default using Tensorboard config'''
     def __init__(self,
-                 tensorboard_config: "TensorboardConfig" = None,
-                 wandb_config: "WandbConfig" = None):
-        
-        assert tensorboard_config is not None or wandb_config is not None, \
-            "You should assign at least one visualization method !"
+                 logtype: Literal["tensorboard","wandb"] = "tensorboard",
+                 wandb_api: str = None,
+                 wandb_entity: str = None,
+                 wandb_project: str = None,
+                 wandb_group: str = None,
+                 wandb_name: str = None,
+                 wandb_cfg: dict = None,
+                 wandb_reinit: bool = True):
+        super().__init__()
 
-        self._init_tensorboard(tensorboard_config)
-        self._init_wandb(wandb_config)
+        self.logtype = logtype
+        self.wandb_api = wandb_api
+        self.wandb_entity = wandb_entity
+        self.wandb_project = wandb_project
+        self.wandb_group = wandb_group
+        self.wandb_name = wandb_name
+        self.wandb_cfg = wandb_cfg
+        self.wandb_reinit = wandb_reinit
+
+        self.is_initialized = False
 
     @rank0only_decorator
     def _init_tensorboard(self, tensorboard_config: TensorboardConfig):
@@ -96,23 +110,48 @@ class VisualizationModule(CheckpointMixin):
 
     @rank0only_decorator
     def update(self, logs_dict:dict):
-        global_step = logs_dict.pop('global_step')
-        logging_steps = logs_dict.pop('logging_steps')
+        global_step = self.global_step
+        logging_steps = self.logging_steps
         self._update_tensorboard(logs_dict, global_step, logging_steps)
         self._update_wandb(logs_dict, global_step, logging_steps)
 
 
-    def get_saved_sub_dir(self): return "tensorabord"
+    def get_saved_sub_dir(self): 
+        return self.logtype
 
     @rank0only_decorator
     def save_ckpt(self, save_dir, tag):
         #TODO: save in save_dir/tensorboard
+        return
         return super().save_ckpt(save_dir, tag)
 
     @rank0only_decorator
     def load_ckpt(self, saved_dir, tag):
-        #TODO: load from saved_dir/tensorborad
-        return super().load_ckpt(saved_dir, tag)
+        self.logging_steps = self.checkpoint_config.visulization_interval
+        if self.logtype == 'tensorboard':
+            self._init_tensorboard(
+                TensorboardConfig(
+                    log_dir = dirname(saved_dir),
+                    name = basename(saved_dir)
+                )
+            )
+            self.wandb_config = None
+        elif self.logtype == "wandb":
+            self._init_wandb(
+                WandbConfig(
+                    api_key = self.wandb_api,
+                    entity = self.wandb_entity,
+                    project = self.wandb_project,
+                    group = self.wandb_group,
+                    name = self.wandb_name,
+                    config = self.wandb_cfg,
+                    reinit = self.wandb_reinit
+
+                )
+            )
+            
+            self.tensorboard_config = None
+        self.is_initialized = True
 
     @rank0only_decorator
     def release(self):
